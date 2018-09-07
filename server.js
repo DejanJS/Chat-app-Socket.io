e181bae1bf14b2761b7ddfae1f27331f404b0ab8
DejanJS <dejan.alvadzijevic@hotmail.rs>
2018-09-07 15:36:27 +0200

    so many things....

--- server.js
+++ server.js
@@ -24,54 +24,105 @@ app.use(body.urlencoded({
 }))
 
 // CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 	next();
-  });
+});
 
-  //post route
-app.post('/user/entry',function(req,res){
-	db.one('SELECT NULLIF(MAX(ID)+1,1) as NewId FROM Users')
-		.then(data => {
-			db.none('INSERT INTO Users(id,username,pass) VALUES($1,$2,$3)', [data.newid, req.body.name, req.body.pass])
-				.then(() => {
-					res.status(200).json({
-						message: "Succesfully registred!"
-					})
-				})
-				.catch(() => {
-					res.status(404).json({
-						message: "Sorry something went wrong."
-					})
-				})
-		});
-	})
+//post route
+app.post('/user/entry',async function (req, res) {
+	let name = req.body.name;
+	let pass = req.body.pass;
+	if(await UserExists(name)){
+		if(await AuthUser(name,pass)){
+			res.status(200).json({
+				message:"Successfully logged in"
+			})
+		} else{
+			res.status(401).json({
+				message:"bad user/pass...try again!"
+			})
+		}
+	} else if(await InsertUser(name,pass)){
+		res.status(200).json({
+			message:"Successfully registred"
+		})
+	} else{
+		res.status(500).json({
+			message:"who knows what happened ?"
+		})
+	}
+})
 
- app.get("/",function(req,res){
- 	res.sendFile(__dirname + "/public/index.html");
- })
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/public/index.html");
+})
 
-function crypting(pass,alg="sha256",secret ="this is my secret",salt = "asjdkluhjsio8djkasn"){
-	return crypto.createHmac(alg,secret).update(`${salt}-${pass}`).digest("hex");
+// function crypting(pass,alg="sha256",secret ="this is my secret",salt = "asjdkluhjsio8djkasn"){
+// 	return crypto.createHmac(alg,secret).update(`${salt}-${pass}`).digest("hex");
+// }
+function crypt(pass,salt,iteration = 100000,keylen = 64,alg="sha256"){
+	return crypto.pbkdf2Sync(pass,Buffer.from(salt),iteration,keylen,alg).toString('hex');
+}
+function salt() {
+	let number = crypto.randomBytes(16);
+	let token = number.toString('hex');
+	return token;
 }
 
 var users = 0;
 
-async function getUser(){
-	try{
+async function getUser() {
+	try {
 		var data = await db.any('SELECT username FROM Users');
-		data.map((usr,i)=>{
+		data.map((usr, i) => {
 			console.log(usr.username)
-		})		
-	} catch(e){
-		console.log("there was an error : ",e);
+		})
+	} catch (e) {
+		console.log("there was an error : ", e);
 	}
 };
 
 getUser();
 
+async function InsertUser(name, pass) {
+	var token = salt();
+	var hashpass = crypt(pass,token);
+	var data = await db.one('SELECT NULLIF(MAX(ID)+1,1) as NewId FROM Users');
+	try {
+		await db.none('INSERT INTO Users(id,username,pass,salt) VALUES($1,$2,$3)', [data.newid, name, hashpass,token]);
+		// res.status(200).json({
+		// 	message: "Successfully registred!"
+		// })
+		return true;
+	} catch {
+		// res.status(500).json({
+		// 	message: "Sorry something went wrong."
+		// })
+		return false;
+	}
+}
+
+
+
+async function UserExists(name) {
+	var count = await db.one(`SELECT COUNT(1) as Counter FROM Users WHERE Users.username ='${name}'`);
+	if (count.counter !== 0) {
+		return true;
+	}
+	return false;
+}
+
+async function AuthUser(name, pass) {
+	var auth = await db.one(`SELECT Salt,pass FROM USERS WHERE username = ${name}`);
+	var hashpass = crypt(pass,auth.salt);
+	if(auth.pass === hashpass){
+		return true;
+	}
+	return false;
+}
 
 
 // io.to('games').on("connection",function(socket){
@@ -96,6 +147,3 @@ io.on('connection', function (socket) {
 		socket.broadcast.emit("notyping", empty)
 	})
 });
-
-
-

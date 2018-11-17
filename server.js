e6e056c356c46cae601f648705c47f8170a5179d
DejanJS <dejan.alvadzijevic@hotmail.rs>
2018-11-17 17:32:04 +0100

    refactoring

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const Message = require('./message.js')
+const User = require('./user.js')
 const http = require("http");
 const host = "localhost";
 const express = require("express");
@@ -7,9 +9,8 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app)
 const socket = require("socket.io");
 const io = socket();
-var pgp = require('pg-promise')();
-var db = pgp('postgres://postgres:grimftw@localhost:5432/Chat');
-const crypto = require("crypto");
+
+
 
 io.listen(server);
 server.listen(port, function () {
@@ -38,13 +39,13 @@ app.post('/user/entry',async function (req, res) {
 	let sockid = req.body.id;
 	let obj = {uname: name,id:sockid};
 	try{
-		if(!await UserExists(name)){
-			await InsertUser(name,pass)
+		if(!await User.exists(name)){
+			await User.insert(name,pass)
 			? userOnlineResponse(obj,"Successfully registred",res)
 			: msgResponse(500,"who knows what happened ?",res)
 			return;
 		}
-		await AuthUser(name,pass)
+		await User.auth(name,pass)
 		? userOnlineResponse(obj,"Successfully logged in",res)
 		:msgResponse(401,"bad user/pass...try again!",res)
 	} catch(e){
@@ -76,7 +77,7 @@ app.get('/svc/curruser',function(req,res){
 app.get("/svc/messages", async function (req, res) {
 	try {
 		res.status(200).json(
-			await GetMsg()
+			await Message.get()
 		)
 	} catch (e) {
 		res.status(500).json({
@@ -86,95 +87,13 @@ app.get("/svc/messages", async function (req, res) {
 	}
 })
 
-function crypt(pass,salt,iteration = 100000,keylen = 64,alg="sha256"){
-	return crypto.pbkdf2Sync(pass,Buffer.from(salt),iteration,keylen,alg).toString('hex');
-}
-function salt() {
-	let number = crypto.randomBytes(16);
-	let token = number.toString('hex');
-	return token;
-}
-
 var users = 0;
 
-async function getUser() {
-	try {
-		var data = await db.any('SELECT username FROM Users');
-		data.map((usr, i) => {
-			console.log(usr.username)
-		})
-	} catch(e) {
-		console.log("there was an error : ", e);
-	}
-};
-
-getUser();
-
-async function InsertUser(name, pass) {
-	console.log("insert ",name,pass);
-	var token = salt();
-	var hashpass = crypt(pass,token);
-	var data = await db.one('SELECT COALESCE(MAX(ID)+1,1) as NewId FROM Users');
-	try {
-		await db.none("INSERT INTO Users(id,username,pass,salt) VALUES($1,$2,$3,$4)", [data.newid, name, hashpass,token]);
-		// res.status(200).json({
-		// 	message: "Successfully registred!"
-		// })
-		return true;
-	} catch(e) {
-		// res.status(500).json({
-		// 	message: "Sorry something went wrong."
-		// })
-		console.log("this is errrorr ",e);
-		return false;
-	}
-}
-
-
-
-async function UserExists(name) {
-	console.log("exist ?",name);
-	var count = await db.one('SELECT COUNT(1) as Counter FROM Users WHERE Users.username = $1',name);
-	console.log("count ",count)
-	if (Number(count.counter) !== 0) {
-		return true;
-	}
-	return false;
-}
-
-async function AuthUser(name, pass) {
-	console.log('auth ',name,pass);
-	var auth = await db.one('SELECT Salt,pass FROM USERS WHERE username = $1',name);
-	var hashpass = crypt(pass,auth.salt);
-	console.log("this is i am looking for ",auth.pass,hashpass);
-	if(auth.pass.trim() === hashpass){
-		return true;
-	}
-	return false;
-}
-
-async function GetId(user){
-	return (await db.one("SELECT id FROM Users WHERE username = $1",user)).id;
-}
-
-
-async function InsertMsg(user,msg){
-	var id = await GetId(user);
-	await db.none('INSERT INTO Messages(UserId,text,timestamp) VALUES($1,$2,current_timestamp)',[id,msg])
-}
-async function GetMsg() {
-	return await db.any(`
-	SELECT 
-		Messages.id,
-		Users.username,
-		Messages.text,
-		Messages.timestamp
-	FROM Messages JOIN Users ON Users.id = Messages.UserId
-	`)
-}
+User.printAll();
 
 var userSocket = [];
 var currUser;
+
 //specific responder for userlogin event
 function userOnlineResponse(connectInfo,msg,res){
 	userSocket.push(connectInfo);
@@ -206,7 +125,7 @@ io.on('connection', function (socket) {
 		console.log('data: ', data);
 		io.emit("chat message", data);
 		try {
-			await InsertMsg(data.user, data.msg);
+			await Message.insert(data.user, data.msg);
 		} catch (e) {
 			console.log("inserting message didn't go well.. ", e)
 		}

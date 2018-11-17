9c45bb64a68732ee0e74c1f015732c5a6ec51e65
DejanJS <dejan.alvadzijevic@hotmail.rs>
2018-11-17 17:32:37 +0100

    message module

---
+++ message.js
@@ -0,0 +1,17 @@
+const db = require('./db.js').db;
+module.exports = class Message{
+    static async insert(user, msg) {
+        var id = await GetId(user);
+        await db.none('INSERT INTO Messages(UserId,text,timestamp) VALUES($1,$2,current_timestamp)', [id, msg])
+    }
+    static async get() {
+        return await db.any(`
+        SELECT 
+            Messages.id,
+            Users.username,
+            Messages.text,
+            Messages.timestamp
+        FROM Messages JOIN Users ON Users.id = Messages.UserId
+        `)
+    }
+}

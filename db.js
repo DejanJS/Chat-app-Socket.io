1ae93ab293cec93e52127ea7aad8952144507439
Dejan <dejan.alvadzijevic@hotmail.rs>
2020-03-04 10:46:25 +0100

    update db.js

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 const pgp = require('pg-promise')();
-const db = pgp('postgres://postgres:grimftw@localhost:5432/Chat');
+const db = pgp('postgres://postgres:yourpassgoeshere@localhost:5432/Chat');
 module.exports = {pgp,db};
